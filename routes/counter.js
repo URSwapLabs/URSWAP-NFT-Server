55e8e0ee518ad2ac26d9e3be38a75ca795c8ee4d
Fahad2122 <fahadjutt2122@gmail.com>
2025-01-16 01:14:18 +0500

    payment added

--- routes/counter.js
+++ routes/counter.js
@@ -7,26 +7,21 @@ router.post('/updateCounter', async (req, res) => {
     const { walletAddress, imageURI } = req.body;
 
     try {
-        if(!walletAddress) {
+        if (!walletAddress) {
             return res.status(400).json({ success: false, message: "Invalid input data" });
         }
 
         let counter = await Counter.findOne({ walletAddress });
 
-        if(!counter) {
-            counter = new Counter({
-                walletAddress,
-                nftGenerations: 1,
-                imageURI: imageURI
-            })
-            await counter.save();
+        if (!counter) {
+            return res.status(404).json({ success: false, message: "Not Found" });
         } else {
             // if(counter.nftGenerations >= process.env.NFT_LIMIT) {
             //     return res.status(400).json({ success: false, message: "Limit Reached, Please mint NFT to continue" });
             // }
             counter.nftGenerations += 1;
             counter.imageURI = imageURI;
-            if(counter.nftGenerations >= process.env.NFT_LIMIT) {
+            if (counter.nftGenerations >= process.env.NFT_LIMIT) {
                 counter.isPaid = false;
             }
             await counter.save();
@@ -44,20 +39,27 @@ router.post("/setPaid", async (req, res) => {
     const { walletAddress } = req.body;
 
     try {
-        if(!walletAddress) {
+        if (!walletAddress) {
             return res.status(400).json({ success: false, message: "Invalid input data" });
         }
 
         let counter = await Counter.findOne({ walletAddress });
 
-        if(!counter) {
-            return res.status(404).json({ success: false, message: "Not Found" });
+        if (!counter) {
+            counter = new Counter({
+                walletAddress,
+                nftGenerations: 1,
+                imageURI: imageURI,
+                isPaid: true
+            })
+            await counter.save();
+        } else {
+            counter.isPaid = true;
+            await counter.save();
         }
 
-        counter.isPaid = true;
-        
         return res.status(200).json({ success: true, message: "$UR Paid Successfully" });
-        
+
     } catch (error) {
         console.log("Error, while getting counter");
         return res.status(500).json({ success: false, message: "Internal server error" });
@@ -66,15 +68,15 @@ router.post("/setPaid", async (req, res) => {
 
 router.post("/clearCounter", async (req, res) => {
     const { walletAddress } = req.body;
-    
+
     try {
-        if(!walletAddress) {
+        if (!walletAddress) {
             return res.status(400).json({ success: false, message: "Invalid input data" });
         }
 
         let counter = await Counter.findOne({ walletAddress });
 
-        if(!counter) {
+        if (!counter) {
             return res.status(404).json({ success: false, message: "Not Found" });
         }
 
@@ -95,13 +97,13 @@ router.post("/getCounter", async (req, res) => {
     const { walletAddress } = req.body;
 
     try {
-        if(!walletAddress) {
+        if (!walletAddress) {
             return res.status(400).json({ success: false, message: "Invalid input data" });
         }
 
         let counter = await Counter.findOne({ walletAddress });
 
-        if(!counter) {
+        if (!counter) {
             return res.status(404).json({ success: false, message: "Not Found" });
         }
 
