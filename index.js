9767fe675e589eb96137e78620368a53320df71f
Fahad2122 <fahadjutt2122@gmail.com>
2025-04-14 15:00:41 +0500

    Updated to Production

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-    // origin: 'https://urswap-marketplace.vercel.app',
+    origin: 'https://urswap-marketplace.vercel.app',
     // origin: 'http://localhost:3000',
-    origin: `http://localhost:5116`,
+    // origin: `http://localhost:5116`,
     credentials: true
 }));
 app.use(express.json());
