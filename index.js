695877320adf55782a86eda00024d0f215c2e3ed
Fahad2122 <fahadjutt2122@gmail.com>
2025-04-22 21:03:07 +0500

    Updated to Development 3000

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const app = express();
 
 app.use(cors({
     // origin: 'https://urswap.io/',
-    origin: 'https://urswap-marketplace.vercel.app',
-    // origin: 'http://localhost:3000',
+    // origin: 'https://urswap-marketplace.vercel.app',
+    origin: 'http://localhost:3000',
     // origin: `http://localhost:5116`,
     credentials: true
 }));
