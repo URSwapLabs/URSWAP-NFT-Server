28d0cc37fc73bbb70e668a8e94c1178879062b7b
Fahad2122 <fahadjutt2122@gmail.com>
2025-05-19 21:04:22 +0500

    Updated to Prodution

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-    // origin: 'https://nft.urswap.io/',
-    origin: 'http://localhost:3000',
+    origin: 'https://nft.urswap.io',
+    // origin: 'http://localhost:3000',
     credentials: true
 }));
 app.use(express.json());
