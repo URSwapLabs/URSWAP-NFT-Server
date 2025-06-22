5a99ecdad7370e7442b42cb279ddd75ee62ed00f
Fahad2122 <fahadjutt2122@gmail.com>
2025-06-22 23:31:30 +0500

    MongoDB Initialized

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI_MAIN, {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
